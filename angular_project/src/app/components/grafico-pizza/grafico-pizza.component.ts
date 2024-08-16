80dfccb08850e838fa71e5e495e7e7a0a0feee65
Alisson Deivison <uepbalisson@gmail.com>
2024-08-16 17:32:33 -0300

    feat: Atualizar componente de gráfico pizza com dados dinâmicos

--- angular_project/src/app/components/grafico-pizza/grafico-pizza.component.ts
+++ angular_project/src/app/components/grafico-pizza/grafico-pizza.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, OnChanges } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafico-pizza',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './grafico-pizza.component.html',
-  styleUrl: './grafico-pizza.component.scss'
+  styleUrl: './grafico-pizza.component.scss',
 })
-export class GraficoPizzaComponent {
-data: any;
+export class GraficoPizzaComponent implements OnChanges{
 
+  @Input() valoresGrafico: number[] = [];
+  @Input() labelsGrafico: string[] = [];
+
+  public pieChartType: ChartType = 'pie';
+  public pieChartData: ChartData<'pie'> = {
+    labels: [],
+    datasets: [
+      {
+        data: []
+      }
+    ]
+  };
+  public pieChartOptions: any = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      }
+    }
+  };
+
+  ngOnChanges(): void {
+    this.updateChartData();
+  }
+
+  private updateChartData(): void {
+    this.pieChartData = {
+      labels: this.labelsGrafico,
+      datasets: [
+        {
+          data: this.valoresGrafico
+        }
+      ]
+    };
+  }
 }
