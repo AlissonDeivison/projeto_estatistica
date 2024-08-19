330f58d498d9817fb7a797df6bd3ff95bf44bd3a
Alisson Deivison <uepbalisson@gmail.com>
2024-08-19 19:49:23 -0300

    feat: Adicionar rotas para as páginas de caso de uso e intervalos

--- angular_project/src/app/app.routes.ts
+++ angular_project/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CasoDeUsoComponent } from './pages/caso-de-uso/caso-de-uso.component';
+import { IntervalosComponent } from './pages/intervalos/intervalos.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'caso-de-uso', component: CasoDeUsoComponent},
+    {path: 'intervalos', component: IntervalosComponent},
 ];
