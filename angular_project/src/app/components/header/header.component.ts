f5a7eb9e3fc76a52bd71f40e6ba05bddd003d17d
Alisson Deivison <uepbalisson@gmail.com>
2024-08-19 17:22:26 -0300

    feat: Atualizar componente de cabeçalho com botões de menu e logout

--- angular_project/src/app/components/header/header.component.ts
+++ angular_project/src/app/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import 'material-icons/iconfont/material-icons.css';
-
-
+import { SidebarService } from '../../services/sidebar.service';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
+import { ServicesModule } from '../../services/services.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ServicesModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isOpen = false;
+  constructor(private sidebarService: SidebarService, private router: Router, private dataService: DataService) {}
+
+  ngOnInit() {}
+  toggleSidebar() {
+    this.sidebarService.toggleSidebar();
+  }
+  logout() {
+    this.dataService.logout();
+    this.router.navigate(['']);
+  }
 
 }
