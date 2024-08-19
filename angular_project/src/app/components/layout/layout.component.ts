5d37ce11611dabad755e52e12c18c88c816ab22e
Alisson Deivison <uepbalisson@gmail.com>
2024-08-19 17:22:36 -0300

    feat: Atualizar layout adicionando barra lateral e container principal

--- angular_project/src/app/components/layout/layout.component.ts
+++ angular_project/src/app/components/layout/layout.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { FooterComponent } from "../footer/footer.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { SidebarService } from '../../services/sidebar.service';
+import { ContainerComponent } from "../container/container.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SidebarComponent, ContainerComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  isOpen = false;
+  constructor(private sidebarService: SidebarService) {}
 
+  ngOnInit() {
+    this.sidebarService.sidebarOpen$.subscribe((isOpen) => {
+      this.isOpen = isOpen;
+    });
+  }
 }
