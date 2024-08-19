2de8245aa90d2374c925bc07e76bf691fcd8b42b
Alisson Deivison <uepbalisson@gmail.com>
2024-08-19 17:23:51 -0300

    feat: Adicionar serviço de barra lateral

--- angular_project/src/app/services/data.service.ts
+++ angular_project/src/app/services/data.service.ts
@@ -1,16 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  
   constructor(private http: HttpClient) {}
+  
 
   private apiUrl:string = 'http://localhost:5000/api';
 
   getdata(endPoint:string){
     return this.http.get(`${this.apiUrl}/${endPoint}`);
+  }
+
+  postdata(nome: string, senha: string, endPoint: string){
+    return this.http.post(`${this.apiUrl}/${endPoint}`, {nome, senha});
+  }
+
+
+  logout(){
+    localStorage.removeItem('token');
   }
 }
---
+++ angular_project/src/app/services/sidebar.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+  private sidebarOpen = new BehaviorSubject<boolean>(false);
+  sidebarOpen$ = this.sidebarOpen.asObservable();
+
+  toggleSidebar() {
+    this.sidebarOpen.next(!this.sidebarOpen.value);
+  }
+  constructor() { }
+}
