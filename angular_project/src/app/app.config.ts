7367ad3d65bcea2a1158131c961c1ec3a46fdedc
Alisson Deivison <uepbalisson@gmail.com>
2024-08-19 17:21:51 -0300

    update: Atualza o arquivo de configurações do projeto  adiciona a rota de login

--- angular_project/src/app/app.config.ts
+++ angular_project/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideCharts(withDefaultRegisterables()), provideCharts(withDefaultRegisterables())]
+  providers: [provideRouter(routes), provideCharts(withDefaultRegisterables()), provideCharts(withDefaultRegisterables()), provideAnimationsAsync()]
 };
--- angular_project/src/app/app.routes.ts
+++ angular_project/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: LoginComponent},
+    {path: 'home', component: HomeComponent}
 ];
