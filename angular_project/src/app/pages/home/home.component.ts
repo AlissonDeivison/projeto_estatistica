e36b881163a314d4f587ec1dbe8de7142f07908d
Alisson Deivison <uepbalisson@gmail.com>
2024-08-20 08:40:59 -0300

    feat: Realiza a limpeza da página inicial

--- angular_project/src/app/pages/home/home.component.ts
+++ angular_project/src/app/pages/home/home.component.ts
@@ -7,41 +7,28 @@ import { CommonModule } from '@angular/common';
 import { Intervalo } from '../../interfaces/intervalo';
 import { GraficoPizzaComponent } from "../../components/grafico-pizza/grafico-pizza.component";
 import { LayoutComponent } from "../../components/layout/layout.component";
-import { CardComponent } from "./card/card.component";
 import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ServicesModule, CommonModule, GraficoPizzaComponent, LayoutComponent, CardComponent],
+  imports: [HeaderComponent, FooterComponent, ServicesModule, CommonModule, GraficoPizzaComponent, LayoutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
   isOpen = false;
-  user : any;
-  
-  public data: Intervalo[] = [];
-  public labelsDoGrafico: string[] = [];
-  public valoresDoGrafico: number[] = [];
 
   constructor(private dataService: DataService, private sidebarService: SidebarService) {}
 
   ngOnInit() {
-    this.carregarDados();
+  
     this.sidebarService.sidebarOpen$.subscribe((isOpen) => {
       this.isOpen = isOpen;
     });
   }
   
-  carregarDados() {
-    this.dataService.getdata('intervalos/idade').subscribe((res: any) => {
-      this.data = res;
-      this.labelsDoGrafico = this.data.map((item) => item.intervalo);
-      this.valoresDoGrafico = this.data.map((item) => item.totalDeItens);
-      console.log(this.labelsDoGrafico);
-    });
-  }
+
  
 
 }
