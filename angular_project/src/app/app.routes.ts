a37ba5be9453c155d1c474040bded8f8550e81de
Alisson Deivison <uepbalisson@gmail.com>
2024-08-20 17:51:34 -0300

    feat: Atualizar rota da página inicial para exibir a página de intervalos

--- angular_project/src/app/app.routes.ts
+++ angular_project/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CasoDeUsoComponent } from './pages/caso-de-uso/caso-de-uso.component';
 import { IntervalosComponent } from './pages/intervalos/intervalos.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'home', component: HomeComponent},
+    {path: 'home', component: IntervalosComponent},
     {path: 'caso-de-uso', component: CasoDeUsoComponent},
-    {path: 'intervalos', component: IntervalosComponent},
 ];
