f97a3e4f8a8e0b98be2312a26a9130b5ffce4e34
Alisson Deivison <uepbalisson@gmail.com>
2024-08-21 11:58:15 -0300

    feat: Adicionar página de mídia ao aplicativo

--- angular_project/src/app/app.routes.ts
+++ angular_project/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CasoDeUsoComponent } from './pages/caso-de-uso/caso-de-uso.component';
 import { IntervalosComponent } from './pages/intervalos/intervalos.component';
+import { MediaComponent } from './pages/media/media.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'home', component: IntervalosComponent},
     {path: 'caso-de-uso', component: CasoDeUsoComponent},
+    {path: 'media', component: MediaComponent}
 ];
