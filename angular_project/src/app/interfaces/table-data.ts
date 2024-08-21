72a1832f7dc4ae5cdd07bf754e850a720e6206e6
Alisson Deivison <uepbalisson@gmail.com>
2024-08-21 19:57:35 -0300

    feat: Adicionar interface de dados da tabela

---
+++ angular_project/src/app/interfaces/table-data.ts
@@ -0,0 +1,9 @@
+export interface TableData {
+    numero_da_classe: number;
+    intervalo_de_classe: string;
+    frequencia: number;
+    ponto_medio: number;
+    frequencia_vezes_ponto_medio: number;
+    limiteInferior: number;
+    limiteSuperior: number;
+}
