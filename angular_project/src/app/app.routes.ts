adbdf79cf7b9012c12dced3e49367bd9402b7ee9
Alisson Deivison <uepbalisson@gmail.com>
2024-08-21 15:01:35 -0300

    feat: Adicionar rota para a página de detalhamento dos cálculos

--- angular_project/src/app/app.routes.ts
+++ angular_project/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { CasoDeUsoComponent } from './pages/caso-de-uso/caso-de-uso.component';
 import { IntervalosComponent } from './pages/intervalos/intervalos.component';
 import { MediaComponent } from './pages/media/media.component';
+import { DetalhamentoDosCalculosComponent } from './pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'home', component: CasoDeUsoComponent},
-    {path: 'resumo', component: IntervalosComponent},
-    {path: 'algoritmos', component: MediaComponent}
+  { path: '', component: LoginComponent },
+  { path: 'home', component: CasoDeUsoComponent },
+  { path: 'resumo', component: IntervalosComponent },
+  { path: 'algoritmos', component: MediaComponent },
+  { path: 'detalhes', component: DetalhamentoDosCalculosComponent },
 ];
