69ec5404f7f13917422d8c950a6f7e3ac73bbdb3
Alisson Deivison <uepbalisson@gmail.com>
2024-08-21 19:58:49 -0300

    feat: Adicionar média de idade dos entrevistados na página de detalhamento dos cálculos

--- angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
+++ angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { LayoutComponent } from "../../components/layout/layout.component";
+import { Component, OnInit } from '@angular/core';
+import { LayoutComponent } from '../../components/layout/layout.component';
+import { ServicesModule } from '../../services/services.module';
+import { DataService } from '../../services/data.service';
+import { TableData } from '../../interfaces/table-data';
+import { TabelaComponent } from './tabela/tabela.component';
 
 @Component({
   selector: 'app-detalhamento-dos-calculos',
   standalone: true,
-  imports: [LayoutComponent],
+  imports: [LayoutComponent, ServicesModule, TabelaComponent],
   templateUrl: './detalhamento-dos-calculos.component.html',
-  styleUrl: './detalhamento-dos-calculos.component.scss'
+  styleUrl: './detalhamento-dos-calculos.component.scss',
 })
-export class DetalhamentoDosCalculosComponent {
+export class DetalhamentoDosCalculosComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(): void {}
 }
