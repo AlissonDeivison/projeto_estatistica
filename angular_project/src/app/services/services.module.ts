2f2b024f979e0c27fb4d2da137d98cd6d47dc302
Alisson Deivison <uepbalisson@gmail.com>
2024-08-21 11:58:37 -0300

    feat: Adicionar módulo MatExpansion ao ServicesModule

--- angular_project/src/app/services/services.module.ts
+++ angular_project/src/app/services/services.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from './data.service';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
   ],
-  providers: [DataService, provideHttpClient(withInterceptorsFromDi())]
+  providers: [DataService, provideHttpClient(withInterceptorsFromDi()), MatExpansionModule],
 })
 export class ServicesModule { }
