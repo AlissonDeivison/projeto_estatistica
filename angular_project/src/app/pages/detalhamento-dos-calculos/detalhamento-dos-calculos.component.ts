9cf87d5f7ffbcb907003f1adcd4dca90d5b512b4
Alisson Deivison <uepbalisson@gmail.com>
2024-08-22 17:54:22 -0300

    feat: Adicionar cálculo de mediana e medidas de dispersão na página de detalhamento dos cálculos

--- angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
+++ angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
@@ -5,11 +5,13 @@ import { DataService } from '../../services/data.service';
 import { TableData } from '../../interfaces/table-data';
 import { TabelaComponent } from './tabela/tabela.component';
 import { CalculoDeModaComponent } from "./calculo-de-moda/calculo-de-moda.component";
+import { CalculoDeMedianaComponent } from "./calculo-de-mediana/calculo-de-mediana.component";
+import { VarianciaEDesvioPadraoComponent } from "./variancia-e-desvio-padrao/variancia-e-desvio-padrao.component";
 
 @Component({
   selector: 'app-detalhamento-dos-calculos',
   standalone: true,
-  imports: [LayoutComponent, ServicesModule, TabelaComponent, CalculoDeModaComponent],
+  imports: [LayoutComponent, ServicesModule, TabelaComponent, CalculoDeModaComponent, CalculoDeMedianaComponent, VarianciaEDesvioPadraoComponent],
   templateUrl: './detalhamento-dos-calculos.component.html',
   styleUrl: './detalhamento-dos-calculos.component.scss',
 })
