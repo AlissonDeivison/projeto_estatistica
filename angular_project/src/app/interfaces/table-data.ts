2ca5aa64dfe6f80d4a57444dfcd28a544d3e7101
Alisson Deivison <uepbalisson@gmail.com>
2024-08-22 17:54:14 -0300

    feat: Adicionar propriedade de quadrado_xi_fi na interface de dados da tabela

--- angular_project/src/app/interfaces/table-data.ts
+++ angular_project/src/app/interfaces/table-data.ts
@@ -1,10 +1,11 @@
 export interface TableData {
-    numero_da_classe: number;
-    intervalo_de_classe: string;
-    frequencia: number;
-    ponto_medio: number;
-    frequencia_vezes_ponto_medio: number;
-    limiteInferior: number;
-    limiteSuperior: number;
-    desvio: number;
+  numero_da_classe: number;
+  intervalo_de_classe: string;
+  frequencia: number;
+  ponto_medio: number;
+  frequencia_vezes_ponto_medio: number;
+  limiteInferior: number;
+  limiteSuperior: number;
+  desvio: number;
+  quadrado_xi_fi?: number;
 }
