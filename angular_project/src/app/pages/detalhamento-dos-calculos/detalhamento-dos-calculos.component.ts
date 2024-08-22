05d58e8523791db42a1a70a8ee4f4277ccd4c7de
Alisson Deivison <uepbalisson@gmail.com>
2024-08-22 11:16:25 -0300

    feat: Adicionar cálculo de moda na página de detalhamento dos cálculos

--- angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
+++ angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
@@ -4,11 +4,12 @@ import { ServicesModule } from '../../services/services.module';
 import { DataService } from '../../services/data.service';
 import { TableData } from '../../interfaces/table-data';
 import { TabelaComponent } from './tabela/tabela.component';
+import { CalculoDeModaComponent } from "./calculo-de-moda/calculo-de-moda.component";
 
 @Component({
   selector: 'app-detalhamento-dos-calculos',
   standalone: true,
-  imports: [LayoutComponent, ServicesModule, TabelaComponent],
+  imports: [LayoutComponent, ServicesModule, TabelaComponent, CalculoDeModaComponent],
   templateUrl: './detalhamento-dos-calculos.component.html',
   styleUrl: './detalhamento-dos-calculos.component.scss',
 })
