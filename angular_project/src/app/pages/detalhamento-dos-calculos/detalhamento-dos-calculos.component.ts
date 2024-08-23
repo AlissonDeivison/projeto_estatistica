da0b6544f89fb9e75bb4fafb5c0da430e93b1e24
Alisson Deivison <uepbalisson@gmail.com>
2024-08-23 10:43:23 -0300

    feat: Adicionar cálculo de coeficiente de variação na página de detalhamento dos cálculos

--- angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
+++ angular_project/src/app/pages/detalhamento-dos-calculos/detalhamento-dos-calculos.component.ts
@@ -7,11 +7,12 @@ import { TabelaComponent } from './tabela/tabela.component';
 import { CalculoDeModaComponent } from "./calculo-de-moda/calculo-de-moda.component";
 import { CalculoDeMedianaComponent } from "./calculo-de-mediana/calculo-de-mediana.component";
 import { VarianciaEDesvioPadraoComponent } from "./variancia-e-desvio-padrao/variancia-e-desvio-padrao.component";
+import { CoeficienteDeVariacaoComponent } from "./coeficiente-de-variacao/coeficiente-de-variacao.component";
 
 @Component({
   selector: 'app-detalhamento-dos-calculos',
   standalone: true,
-  imports: [LayoutComponent, ServicesModule, TabelaComponent, CalculoDeModaComponent, CalculoDeMedianaComponent, VarianciaEDesvioPadraoComponent],
+  imports: [LayoutComponent, ServicesModule, TabelaComponent, CalculoDeModaComponent, CalculoDeMedianaComponent, VarianciaEDesvioPadraoComponent, CoeficienteDeVariacaoComponent],
   templateUrl: './detalhamento-dos-calculos.component.html',
   styleUrl: './detalhamento-dos-calculos.component.scss',
 })
